9350c46a6a8709ee4349a5f33c51f844574428d4
Ash Berlin <ash_github@firemirror.com>
2009-04-13 22:10:59 -0700

    Simple change to TAP like output

--- template/test.js
+++ template/test.js
@@ -3,11 +3,23 @@ exports.print = function () {
     sys.print.apply(undefined, arguments);
 };
 
-exports.assert = function (guard, message) {
+exports.assert = function assert(guard, message) {
+    var n = ++assert.state.count;
     if (guard) {
-        exports.print('PASS ' + message, 'pass');
+        exports.print(n + 'ok ' + message, 'pass');
     } else {
-        exports.print('FAIL ' + message, 'fail');
+        exports.print(n + 'not ok ' + message, 'fail');
     }
+};
+
+exports.done_testing = function() {
+  if (assert.state) 
+    exports.print("1.."+assert.state.count);
+  else 
+    exports.print("0..0");
+}
+
+exports.assert.state = {
+  count: 0;
 };
 
